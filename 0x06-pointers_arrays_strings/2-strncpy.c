15c2df7307dc4ed5923e5f8df8910dcef03d0b7f
alejogonza <alejo.1996.2001@gmail.com>
2019-07-04 20:22:22 +0000

    script

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncpy - entry point
+ * Description: copies a string
+ * @dest: destiny
+ * @src: source
+ * @n: num of char
+ * Return: dest + src
+ */
+ 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
