70048172f5ff42a413c53b96b658195562a9a32d
alejogonza <alejo.1996.2001@gmail.com>
2019-07-04 19:15:23 +0000

    script

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcat - entry point
+ * Description: concatenate two string
+ * @dest: O.M
+ * @src: new string
+ * Return: dest + src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 6;
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i + 1] = '\0';
+
+	return (dest);
+}
