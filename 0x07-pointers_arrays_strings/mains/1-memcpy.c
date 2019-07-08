646cc2f14a083afbf983d88b7aa1c6372043a678
alejogonza <alejo.1996.2001@gmail.com>
2019-07-08 17:14:28 +0000

    script

---
+++ 0x07-pointers_arrays_strings/mains/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* _memcpy - entry point
+* Description: num of bytes from a src - dest
+* @src: cp from
+* @n: c-bytes
+* @dest: cp to
+* Return: Pointer dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int memy;
+    unsigned int var1;
+
+	for (memy = 1; memy <= n; memy++)
+            var1 = memy - 1;
+		*(dest + var1) = *(src + var1);
+
+	return (dest);
+}
