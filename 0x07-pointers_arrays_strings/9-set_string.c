2ee63a1adafaf1d24889affa58a348a087831883
alejogonza <alejo.1996.2001@gmail.com>
2019-07-08 23:11:16 +0000

    script

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - entry point
+ * Description: sets value pointer = char
+ * @s: x2 pointer
+ * @to: *p to char
+ * Return: none
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
