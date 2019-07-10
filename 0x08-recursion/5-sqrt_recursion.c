2764016c448eccff64bf59f3c009f9dd8e1a3b55
alejogonza <alejo.1996.2001@gmail.com>
2019-07-10 18:31:54 +0000

    script

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,8 +4,6 @@
  * _sqrt_recursion - entry point
  * Description: returns natural square root of a number
  * @n: int
- * @i: int
- * @j: int
  * Return: n square root -1 if no square root
 */
 int _sqrt_recursion(int n)
@@ -13,6 +11,13 @@ int _sqrt_recursion(int n)
 	return (sqrt_nl_root(n, 1));
 }
 
+/**
+ * sqrt_nl_root - entry point
+ * Description: square root part
+ * @i: int
+ * @j: int
+ * Return: square root
+ */
 int sqrt_nl_root(int i, int j)
 {
 	int square = j * j;
