dbab021f58ecfbb00b98fb3a4bf65180a8021cab
alejogonza <alejo.1996.2001@gmail.com>
2019-07-13 18:27:12 +0000

    script

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,48 @@
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
+#include "stdlib.h"
 /**
- * main - prints the minimum number of coins to make change for an amount.
- * of money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - entry point
+ * Description: prints the minimum num of coins to change for an amount
+ * @argc: the nums of args
+ * @argv: the array with the args
+ * Return: 0 or 1 if the args are not nums
  */
+
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int counter;
+	int nums;
+	int sup;
 
-	if (argc == 1 || argc > 2)
+	sup = 0;
+
+	int start[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
-	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		nums = atoi(argv[1]);
+		if (nums < 0)
+		{
+			printf("0\n");
+			return (1);
+		}
+		for (counter = 0; start[counter]; counter++)
+		{
+		if (nums >= start[counter])
+		{
+			sup = sup + (nums / start[counter]);
+			nums = nums % start[counter];
+		}
+		if (nums == 0)
+		{
+			break;
+		}
 	}
-	printf("%d\n", ncoins);
-	return (0);
+		printf("%d\n", sup);
+		return (0);
 }
