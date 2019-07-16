21fb201a13fd06ffcc93b1875984d25feb59fc97
alejogonza <alejo.1996.2001@gmail.com>
2019-07-16 17:18:04 +0000

    script

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,34 +9,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *pointer = NULL;
-        int a;
-        int b;
-        int c;
-        c = 0;
+	char *pointer = NULL;
+	int a;
+	int b;
+	int c;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	c = 0;
 
-        for (a = 0; s1[a]; a++)
-                ;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        for (b = 0; s2[b]; b++)
-                ;
+	for (a = 0; s1[a]; a++)
+		;
 
-        pointer = malloc((a + b + 1) * sizeof(char));
-        if (pointer == NULL)
-                return (NULL);
+	for (b = 0; s2[b]; b++)
+		;
 
-        for (c = 0; s1 && s1[c]; c++)
-                pointer[c] = s1[c];
-        pointer[c] = '\0';
+	pointer = malloc((a + b + 1) * sizeof(char));
+	if (pointer == NULL)
+		return (NULL);
 
-        for (c = 0; s2 && s2[c]; c++)
-                pointer[a + c] = s2[c];
-        pointer[a + c] = '\0';
+	for (c = 0; s1 && s1[c]; c++)
+		pointer[c] = s1[c];
+	pointer[c] = '\0';
 
-        return (pointer);
+	for (c = 0; s2 && s2[c]; c++)
+		pointer[a + c] = s2[c];
+	pointer[a + c] = '\0';
+
+	return (pointer);
 }
