b3b8ae27077bae278b50a7e8956b7ea3d9407d56
alejogonza <alejo.1996.2001@gmail.com>
2019-07-18 20:21:24 +0000

    script

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,18 +11,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int mloc;
-	unsigned int *ary;
+	char *mp;
+	unsigned int armem;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ary = (unsigned int *)malloc(nmemb * size * sizeof(int));
-	if (ary == NULL)
-	{
-		free(ary);
+
+	mp = malloc(nmemb * size);
+
+	if (mp == NULL)
 		return (NULL);
-	}
-	for (mloc = 0; mloc <= size * nmemb; mloc++)
-		ary[mloc] = 0;
-	return (ary);
+
+	for (armem = 0; armem < (nmemb * size); armem++)
+		mp[armem] = 0;
+
+	return (mp);
 }
