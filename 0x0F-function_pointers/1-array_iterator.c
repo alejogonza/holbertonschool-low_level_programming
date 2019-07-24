15affcc1131b07cae15bbea9984c2536d41d5db7
alejogonza <alejo.1996.2001@gmail.com>
2019-07-24 18:15:12 +0000

    [update] update function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - entry point
+ * Description: function given as a parameter on each element of an array
+ * @array:  array
+ * @size: Size of array
+ * @action: Function
+ * Return: Void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
