00a78e2c032248c181f845b75a3fbc7760ca58b3
alejogonza <alejo.1996.2001@gmail.com>
2019-07-24 15:59:50 +0000

    [update] upload the lib functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,4 +11,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /** FUNCTION_POINTERS */
+#endif /**FUNCTION_POINTERS*/
