db5e3ef57055d9c5ce168032e452419a2d678118
alejogonza <alejo.1996.2001@gmail.com>
2019-07-31 21:02:12 +0000

    [fix] fix file

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,28 +1,25 @@
+section .text
+
+	global main
 
 
-section .text                   ;section declaration
+main:
+	mov	rax, 1
 
-                                ;we must export the entry point to the ELF linker or
-    global  _start              ;loader. They conventionally recognize _start as their
-			                          ;entry point. Use ld -e foo to override the default.
+	mov	rdi, 1
 
-_start:
+	mov	rsi, msg
 
-                                ;write our string to stdout
+	mov	rdx, 17
 
-    mov     edx,len             ;third argument: message length
-    mov     ecx,msg             ;second argument: pointer to message to write
-    mov     ebx,1               ;first argument: file handle (stdout)
-    mov     eax,4               ;system call number (sys_write)
-    int     0x80                ;call kernel
+	syscall
 
-                                ;and exit
+	mov	rax, 60
 
-  	mov     ebx,0               ;first syscall argument: exit code
-    mov     eax,1               ;system call number (sys_exit)
-    int     0x80                ;call kernel
+	mov	rdi, 0
 
-section .data                   ;section declaration
+	syscall
 
-msg db      "Hello, Holberton",0xa ;our dear string
-len equ     $ - msg             ;length of our dear string
+section .data
+
+	msg db "Hello, Holberton",`\n`
