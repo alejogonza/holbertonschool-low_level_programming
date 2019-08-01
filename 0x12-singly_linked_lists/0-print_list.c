cf750d5bcc85192361cad0fce3d772700bbf3a38
alejogonza <alejo.1996.2001@gmail.com>
2019-08-01 23:25:12 +0000

    [feature] print number of elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - entry ponit
+ * descripiton: print one single list
+ * @h: pointer to list.
+ * Return: elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int lcount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		lcount++;
+	}
+	return (lcount);
+}
