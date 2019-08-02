3f0fb4b8db4930132733601ce6b8b1a2f8628179
alejogonza <alejo.1996.2001@gmail.com>
2019-08-02 02:28:24 +0000

    [feature] hello word assambler

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+	section .data
+fmt	 db "%s",10,0
+msg	 db "Hello, Holberton", 0
+
+	section .text
+	global main
+	extern printf
+
+main:
+	mov	rax, 0
+	mov	rdi, fmt
+	mov	 esi, msg
+	mov	rdx, 17
+	call	 printf
+
+	mov	rax, 1
+	ret
