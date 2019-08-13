ca73f3d97b75f948b0e3ba4ffdf6169364b4639f
alejogonza <alejo.1996.2001@gmail.com>
2019-08-13 17:27:51 +0000

    [feature] add adv

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,48 +1,49 @@
 #include "holberton.h"
+
 /**
- * print_buffer - entry point
+ * infinite_add - entry point
  * Description: function that adds two numbers.
- * @b: String to print
- * @size: Size of String
+ * @n1: number
+ * @n2: number
+ * @r: Buffer
+ * @size_r: buffer size
+ * Return: add numbers
  */
-
-void print_buffer(char *b, int size)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int axi;
-	int axi2;
-	int axi3;
+	int a1;
+	int a2;
+	int a3;
+	int a4;
+	int a5;
+	int sum;
+	int add;
+	char *a, *b;
 
-	if (size <= 0)
-		printf("\n");
-	else
+	for (a1 = 0; n1[a1] != '\0'; a1++)
+		;
+
+	for (a2 = 0; n2[a2] != '\0'; a2++)
+		;
+
+	(a1 > a2) ? (a = n1, b = n2) : (a = n2, b = n1);
+
+	for (a3 = a1 - 1, a4 = a2 - 1, a5 = 0, add = 0; a3 >= 0 || add != 0; a3--, a4--, a5++)
 	{
-		for (axi = 0; axi < size; axi += 10)
+		sum = (a3 >= 0) ? ((a[a3] - 48) + add) : add;
+		sum = (a4 >= 0) ? ((b[a4] - 48) + sum) : sum;
+
+		if (sum <= 9)
 		{
-			printf("%.8x:", axi);
-
-			for (axi2 = axi; axi2 < axi + 10; axi2++)
-			{
-				if (axi2 % 2 == 0)
-					printf(" ");
-
-				if (axi2 < size)
-					printf("%.2x", b[axi2]);
-				else
-					printf("  ");
-			}
-			printf(" ");
-			for (axi3 = axi; axi3 < axi + 10; axi3++)
-			{
-				if (axi3 >= size)
-				{
-					break;
-				}
-
-				b[axi3] = (b[axi3] >= 0 && b[axi3] <= 31) ? ('.') : (b[axi3]);
-
-				printf("%c", b[axi3]);
-			}
-			printf("\n");
+			add = 0, r[a3 + 1] = sum + 48;
+		}
+		else
+		{
+			add = sum, sum %= 10, add /= 10, r[a3 + 1] = sum + 48;
 		}
 	}
+
+	(a5 == a1) ? (r[a5 + 1] = '\0', r = r + 1) : (r[a5] = '\0', r = r);
+	r = (a5 >= size_r) ? 0 : r;
+	return (r);
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _holberton_h_
 #define _holberton_h_
 
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
