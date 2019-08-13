50dcac5e1512a54ee1c5cac2aa0fde6c8168ea93
alejogonza <alejo.1996.2001@gmail.com>
2019-08-13 15:52:07 +0000

    [feature] function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * create_file - entry point
+ * Description: create a file with permission 600
+ * @filename: name of file
+ * @text_content: data of the file
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int filep;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "this is a secret xd";
+	}
+
+	filep  = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (filep == -1)
+	{
+		return (-1);
+	}
+
+	write(filep, text_content, strlen(text_content));
+
+	return (1);
+}
