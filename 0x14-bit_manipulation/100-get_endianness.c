33ab255e07acbd23780135639d130005e5102ed3
alejogonza <alejo.1996.2001@gmail.com>
2019-08-13 19:26:09 +0000

    [feature] add adv

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * get_endianness - entry point
+ * Descripiton: checks the endianness
+ * Return: 0 and 1
+ */
+int get_endianness(void)
+{
+	int a;
+	a = 1;
+
+	if (*(char *)&a == 1)
+		return (1);
+	else
+		return (0);
+}
