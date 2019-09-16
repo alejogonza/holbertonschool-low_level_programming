2d2ea80d84594e714fe466056e47458ac599e322
alejogonza <alejo.1996.2001@gmail.com>
2019-09-16 23:16:42 +0000

    [fix] .-.

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,6 +1,6 @@
 #ifndef DINAMIC_C
 #define DINAMIC_C
-#include <unistd.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif /* DINAMIC_C */
