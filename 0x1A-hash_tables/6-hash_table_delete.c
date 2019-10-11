5830173370bf49bcf34fc0653d4229be76231242
alejogonza <alejo.1996.2001@gmail.com>
2019-10-11 22:57:43 +0000

    [feature] delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - entry point
+ * @ht: hash table
+ * Return: None
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *actual = NULL, *p = NULL;
+	unsigned long int count = 0;
+
+	if (!ht)
+		return;
+	if (ht->array)
+	{
+		for (count = 0; count < ht->size; count++)
+		{
+			if (ht->array[count])
+			{
+				actual = ht->array[count];
+				while (actual)
+				{
+					p = actual;
+					free(p->key);
+					free(p->value);
+					free(p);
+					actual = actual->next;
+				}
+			}
+		}
+		free(ht->array);
+	}
+	free(ht);
+}
