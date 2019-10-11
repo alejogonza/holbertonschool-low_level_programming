93740e283c02bf4d0eff277f5052ea5325089b43
alejogonza <alejo.1996.2001@gmail.com>
2019-10-11 22:49:29 +0000

    [fix] replant

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,35 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_set - entry point
- * @ht: the hash table
- * @key: the key
- * @value: the val associated with the key
- * Description: function that adds an element to the hash table
- * Return: 1 if it success, 0 otherwise
+ * myStrdup - entry point
+ * @node: node that hold the val
+ * @myKey: string duplicate
+ * Return: * to string
  */
+char *myStrdup(hash_node_t **node, char *myKey)
+{
+	char *stri = NULL;
 
+	stri = strdup(myKey);
+	if (stri == NULL)
+	{
+		if ((*node)->key)
+			free((*node)->key);
+		if ((*node)->value)
+			free((*node)->value);
+		free(*node);
+		return (NULL);
+	}
+	return (stri);
+}
+
+/**
+ * hash_table_set - entry point
+ * @ht: h_table to use
+ * @key: h_table key
+ * @value: val associated with the key
+ * Return: 1 if good
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long index;
-	unsigned long size;
-	hash_node_t *new = NULL;
+	hash_node_t *n = NULL, *act = NULL;
+	char *tmp = NULL;
+	unsigned long int ind = 0;
 
-	if (ht  == NULL || key == NULL || value == NULL)
+	if (!ht || !strcmp(key, "") || !key || !value)
 		return (0);
-
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-
-	new = malloc(sizeof(hash_node_t));
-
-	if (new == NULL)
+	ind = hash_djb2((const unsigned char *)key) % ht->size;
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
 		return (0);
-
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
+	n->key = myStrdup(&n, (char *)key);
+	n->value = myStrdup(&n, (char *)value);
+	n->next = NULL;
+	if (ht->array[ind] == NULL)
+		ht->array[ind] = n;
+	else
+	{
+		act = ht->array[ind];
+		while (act)
+		{
+			if (!strcmp(act->key, key))
+			{
+				tmp = act->value;
+				act->value = myStrdup(&n, (char *)value);
+				free(tmp);
+				free(n->key);
+				free(n->value);
+				free(n);
+				return (1);
+			}
+			act = act->next;
+		}
+		n->next = ht->array[ind];
+		ht->array[ind] = n;
+	}
 	return (1);
 }
